17f881f8a783483853816efa83ed8a69521b190f
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-07-24 10:50:48 +0300

    Add status choices and ordering by status in Task model models.py

--- list/models.py
+++ list/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 
 class Task(models.Model):
+    STATUS_CHOICES = (
+        ("done", "DONE"),
+        ("not done", "NOT DONE"),
+    )
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField()
-    status = models.BooleanField(default=False)
-    tags = models.ManyToManyField("Tag", on_delete=models.CASCADE)
+    status = models.BooleanField(default=False, choices=STATUS_CHOICES)
+    tags = models.ManyToManyField("Tag")
+
+    class Meta:
+        ordering = ["status"]
 
 
 class Tag(models.Model):
