bbbde1f8624c8f9bfac4f71f764008757276bdec
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-07-24 10:30:53 +0300

    Add app name and INTERNAL_IPS to settings

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
 # Application definition
 
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "list",
 ]
 
 MIDDLEWARE = [
