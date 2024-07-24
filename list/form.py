43bf4115bd586f5d8ed952401ec832f380a864e6
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-07-24 14:59:34 +0300

    Add multiply choices of tags in form.py

--- list/form.py
+++ list/form.py
@@ -4,6 +4,11 @@ from list.models import Task, Tag
 
 
 class TaskForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = Task
         fields = ["content", "tags"]
