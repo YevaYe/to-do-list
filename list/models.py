bd6754d54c0531c2ff108bd6d865ead8b3d56c1a
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-07-24 10:30:06 +0300

    Create models Tag and Task

--- list/models.py
+++ list/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from pip._vendor.rich.markup import Tag
 
-# Create your models here.
+
+class Task(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    status = models.BooleanField(default=False)
+    tags = models.ManyToManyField("Tag", on_delete=models.CASCADE)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
