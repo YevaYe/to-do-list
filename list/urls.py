518ffb4cbc013c71f4f02dd9be1d89c2206e74bd
Yeva Yepifanova <yevayepifanova@gmail.com>
2024-07-24 13:25:13 +0300

    Create urls.py file in list app and create CRUD ways of Task and Tags

---
+++ list/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from list.views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView, TagListView, TagCreateView, \
+    TagUpdateView
+
+
+urlpatterns = [
+    path("", TaskListView.as_view(), name="task-list"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("update/<int:pk>/", TaskUpdateView.as_view(), name="task-update"),
+    path("delete/<int:pk>/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/update/<int:pk>/", TagUpdateView.as_view(), name="tag-update"),
+]
+
+app_name = "list"
